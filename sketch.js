00ab8782f8430837a8448c5d83905a8bf919d7ed
Kalpesh Kawalkar <102687995+Kalpesh-Kawalkar@users.noreply.github.com>
2023-08-02 00:16:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,17 @@
+background(200);
+     console.log('Draw function');
+    // 1.Point
+    point(200,200);
+    // 2.Line
+    line(200, 200, 300, 300);
+    // 3.Triangle
+    triangle(100, 200, 200, 300, 250, 300);
+    // 4.Rectangle
+    rect(500, 200, 200, 100);
+    // 5.Square
+    ellipse(400, 300, 300, 100);
+
+
 function setup(params) {
     createCanvas(800, 500);
     console.log('Setup function');
